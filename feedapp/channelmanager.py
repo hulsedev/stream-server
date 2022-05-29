bc24a8ba32a0ba0d0aeae3aad791e27f83b143ad
sachaStewart <sachalevy26@gmail.com>
2022-05-29 19:48:17 -0400

    working local integration test

--- feedapp/channelmanager.py
+++ feedapp/channelmanager.py
@@ -103,7 +103,10 @@ class ChannelAuthManager(DefaultChannelManager):
         event_data = {
             "qid": record.id,
         }
-        event_data.update(**request.GET)
+        # manually unwrap the data from the request
+        event_data.update(
+            {"data": request.GET.get("data"), "task": request.GET.get("task")}
+        )
 
         # send event to producer channel for query
         dje.send_event(
